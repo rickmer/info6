abe319d869ed5d120e746c1afc028fb92c7cbc13
Rickmer Frier <info@rickmerfrier.de>
2014-04-26 15:29:02 +0200

    fixing broken travis build

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(  name='info6',
+        version='1.0',
+        description='just an example',
+        author='rickmer',
+        author_email='me@rickmer.org',
+        url='https://github.com/rickmer/info6',
+)
